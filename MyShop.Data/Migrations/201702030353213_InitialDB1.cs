5ad9d4bcd41df68144f5043c7ad774388a11c3c6
Nguyen Chi Duoc <chiduocse@gmail.com>
2017-02-03 23:01:41 +0700

    add postservice

---
+++ MyShop.Data/Migrations/201702030353213_InitialDB1.cs
@@ -0,0 +1,60 @@
+namespace MyShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDB1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ContactDetails",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 250),
+                        Phone = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 250),
+                        Website = c.String(maxLength: 250),
+                        Address = c.String(maxLength: 250),
+                        Other = c.String(),
+                        Lat = c.Double(),
+                        Lng = c.Double(),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Errors",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Message = c.String(),
+                        StrackTrace = c.String(),
+                        CrearedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Feedbacks",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 250),
+                        Email = c.String(maxLength: 250),
+                        Message = c.String(maxLength: 500),
+                        CreatedDate = c.DateTime(nullable: false),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Feedbacks");
+            DropTable("dbo.Errors");
+            DropTable("dbo.ContactDetails");
+        }
+    }
+}
